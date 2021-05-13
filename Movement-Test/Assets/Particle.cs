25c7769bc943a8b72cf66bb85bf83c81d3e69885
Trottero <wrlxs@hotmail.com>
2021-05-13 14:27:08 +0200

    Clean up particle code and project

--- Movement-Test/Assets/Particle.cs
+++ Movement-Test/Assets/Particle.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
-    public float speed = 1f;
-    public float turningSpeed = 1f;
-    public float perceptionRange = 5f;
-    public bool isHighlighted = false;
+    public float Speed = 5f;
+    public float TurningSpeed = 2.5f;
+    public bool ShowDebugGizmos = false;
 
+    [Header("Swarm Parameters")]
     public float CohesionFactor = 0.25f;
     public float CohesionRange = 5f;
     public float SeperationFactor = 0.25f;
@@ -18,31 +18,35 @@ public class Particle : MonoBehaviour
     public float AlignmentFactor = 0.25f;
     public float AlignmentRange = 5f;
 
+    [Header("Target tracking")]
+    public bool TrackTarget = true;
     public float TargetFactor = 0.25f;
+    public float TargetRange = 5f;
+    public GameObject Target;
+
+
+    [Header("Environment evasion")]
     public float EvasionFactor = 0.25f;
     public float EvasionRange = 5f;
     public int EvasionRaySamples = 100;
-    public bool TrackTarget = true;
+    public MeshCollider EnvironmentCollider;
 
-    private GameObject target;
-    private GameObject[] _neighbours;
-    private MeshCollider _boundingBoxCollider;
-    private Vector3[] _spherePoints;
-    private float _maxRange;
+    // Private variables
+    private GameObject[] neighbours;
+    private Vector3[] spherePoints;
+    private float variableSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.Find("Target");
-        _maxRange = new float[] { CohesionRange, SeperationRange, AlignmentRange, EvasionRange }.Max();
-        _boundingBoxCollider = GameObject.Find("BoundingBox").GetComponent<MeshCollider>();
-        _spherePoints = new Vector3[EvasionRaySamples];
+        Target = Target == null ? GameObject.Find("Target") : Target;
+        EnvironmentCollider = EnvironmentCollider == null ? GameObject.Find("BoundingBox").GetComponent<MeshCollider>() : EnvironmentCollider;
 
-        _neighbours = GameObject.FindGameObjectsWithTag("ParticleGameObject")
-            // Exclude self and calculate distances
+        spherePoints = new Vector3[EvasionRaySamples];
+        variableSpeed = Speed;
+        neighbours = GameObject.FindGameObjectsWithTag("ParticleGameObject")
             .Where(x => x.name != name)
             .ToArray();
-
     }
 
     // Update is called once per frame
@@ -56,13 +60,13 @@ public class Particle : MonoBehaviour
         var evasion = computeEvasion();
 
         // Change our rotation so we avoid other particles
-        var separation = computeSeperation(_neighbours);
+        var separation = computeSeperation();
 
-        // Prefer to move to the center of our neighbours
-        var cohesion = computeCohesion(_neighbours);
+        // Prefer to move to the center of our neighbouring particles
+        var cohesion = computeCohesion();
 
         // Align with other particles
-        var alignment = computeAlignment(_neighbours);
+        var alignment = computeAlignment();
 
         // Calculate the shit
         var optimal_vector = (SeperationFactor * separation + CohesionFactor * cohesion + TargetFactor * targetvec + AlignmentFactor * alignment + EvasionFactor * evasion).normalized;
@@ -86,10 +90,13 @@ public class Particle : MonoBehaviour
         }
 
         // Draws a line to the new optimal trajectory
-        Debug.DrawLine(transform.position, transform.position + optimal_vector, Color.magenta, 0.0f);
+        if (ShowDebugGizmos)
+        {
+            Debug.DrawLine(transform.position, transform.position + optimal_vector, Color.magenta, 0.0f);
+        }
 
         // Calculate Quaternion
-        float singleStep = turningSpeed * Time.deltaTime;
+        float singleStep = TurningSpeed * Time.deltaTime;
         var rotvec = Vector3.RotateTowards(transform.forward, optimal_vector, singleStep, 0.0f);
 
         // Set the rotation
@@ -98,38 +105,49 @@ public class Particle : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        if (isHighlighted)
+        if (ShowDebugGizmos)
         {
-            foreach (var item in _neighbours)
+            // Draw a line to all of its neighbours
+            foreach (var item in neighbours)
             {
                 Debug.DrawLine(transform.position, item.transform.position, Color.green, 0.0F);
             }
-            Gizmos.DrawWireSphere(transform.position, _maxRange);
+            Gizmos.DrawWireSphere(transform.position, new float[] { CohesionRange, SeperationRange, AlignmentRange, EvasionRange }.Max());
         }
     }
 
     private void moveForward()
     {
         // Move ourselves forward in current direction.
-        transform.localPosition += speed * transform.forward * Time.deltaTime;
+        transform.localPosition += variableSpeed * transform.forward * Time.deltaTime;
     }
 
     private Vector3 computeTarget()
     {
-        if (target == null || !TrackTarget)
+        if (Target == null || !TrackTarget)
+        {
+            return Vector3.zero;
+        }
+
+        // Check if target is in range, if it isn't, just return zero vector
+        if (Vector3.Distance(Target.transform.position, transform.position) > TargetRange)
         {
             return Vector3.zero;
         }
 
         // Pad coordinate system with our own localPosition
-        Vector3 targetDirection = target.transform.position - transform.position;
+        Vector3 targetDirection = Target.transform.position - transform.position;
 
         return targetDirection.normalized;
     }
 
-    private Vector3 computeSeperation(GameObject[] neighbours)
+    private bool neighbourInRange(Transform neighbour, float seperationRange)
     {
+        return Vector3.Distance(transform.position, neighbour.position) < seperationRange;
+    }
 
+    private Vector3 computeSeperation()
+    {
         var result = new Vector3();
         for (int i = 0; i < neighbours.Length; i++)
         {
@@ -145,20 +163,8 @@ public class Particle : MonoBehaviour
         return result.normalized;
     }
 
-    private bool neighbourInRange(Transform neighbour, float seperationRange)
+    private Vector3 computeCohesion()
     {
-        return Vector3.Distance(transform.position, neighbour.position) < seperationRange;
-    }
-
-    private Vector3 computeCohesion(GameObject[] neighbours)
-    {
-
-        if (neighbours.Length == 0)
-        {
-            // Avoids division by 0.
-            return Vector3.zero;
-        }
-
         var result = new Vector3();
         var n_neighs = 0;
         for (int i = 0; i < neighbours.Length; i++)
@@ -170,20 +176,20 @@ public class Particle : MonoBehaviour
             }
         }
 
+        if (n_neighs == 0)
+        {
+            // Avoids division by 0.
+            return Vector3.zero;
+        }
+
         // avg vector assuming current fish 0.0
         result /= n_neighs;
 
         return result.normalized;
     }
 
-    private Vector3 computeAlignment(GameObject[] neighbours)
+    private Vector3 computeAlignment()
     {
-        if (neighbours.Length == 0)
-        {
-            // Avoids division by 0.
-            return Vector3.zero;
-        }
-
         var result = new Vector3();
         var n_neighs = 0;
         for (int i = 0; i < neighbours.Length; i++)
@@ -193,6 +199,12 @@ public class Particle : MonoBehaviour
                 result += neighbours[i].transform.forward;
                 n_neighs++;
             }
+        }
+
+        if (n_neighs == 0)
+        {
+            // Avoids division by 0.
+            return Vector3.zero;
         }
 
         // avg vector assuming current fish 0.0
@@ -206,22 +218,24 @@ public class Particle : MonoBehaviour
         // Computes the vector required to avoid obstacles.
 
         // Populate sphere points array with points
-        getPerceptionSphere(EvasionRaySamples, _spherePoints);
+        getPerceptionSphere(spherePoints);
 
         var resultvec = new Vector3();
         var n_collisions = 0;
+        var min_distance = EvasionRange;
 
         // Calculate the sum of the distances between the particle and the collider
         for (int i = 0; i < EvasionRaySamples; i++)
         {
             // Cast ray from fish to point and check if it intersects with an object.
-            var ray = new Ray(transform.position, _spherePoints[i]);
-            if (_boundingBoxCollider.Raycast(ray, out var hitinfo, EvasionRange))
+            var ray = new Ray(transform.position, spherePoints[i]);
+            if (EnvironmentCollider.Raycast(ray, out var hitinfo, EvasionRange))
             {
                 // Add hitinfo position relative to position
                 resultvec += hitinfo.point - transform.position;
                 n_collisions++;
-                if (isHighlighted && n_collisions % 5 == 0)
+
+                if (ShowDebugGizmos)
                 {
                     Debug.DrawLine(transform.position, hitinfo.point, Color.blue, 0.0f);
                 }
@@ -239,7 +253,7 @@ public class Particle : MonoBehaviour
         // Calculate the inverse vector, just like in seperation.
         resultvec = resultvec * -1;
 
-        if (isHighlighted)
+        if (ShowDebugGizmos)
         {
             Debug.DrawLine(transform.position, transform.position + resultvec, Color.cyan);
         }
@@ -247,13 +261,13 @@ public class Particle : MonoBehaviour
         return resultvec.normalized;
     }
 
-    private void getPerceptionSphere(int n_samples, Vector3[] spherePoints)
+    private void getPerceptionSphere(Vector3[] spherePoints)
     {
         // Return all points on the sphere of perception
-        for (int i = 0; i < n_samples; i++)
+        for (int i = 0; i < spherePoints.Length; i++)
         {
-            var phi = Math.Acos(1 - 2 * ((float)i + 0.5) / n_samples);
-            var theta = Math.PI * (1 + Math.Sqrt(5)) * (float)i + 0.5;
+            var phi = Math.Acos(1 - 2 * ((float)i + 0.5) / spherePoints.Length);
+            var theta = Math.PI * (1 + Math.Sqrt(5)) * (float)i + 0.5; // Golden ratio
             spherePoints[i].x = (float)Math.Cos(theta) * (float)Math.Sin(phi);
             spherePoints[i].y = (float)Math.Sin(theta) * (float)Math.Sin(phi);
             spherePoints[i].z = (float)Math.Cos(phi);
